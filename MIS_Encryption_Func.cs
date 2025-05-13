e25002423e2ffb7f83aae9a51296d13f1e9f2d39
Rahul Diwakar <rahul.diwakar@elephant.com>
2025-05-13 08:20:43 -0400

    Update RegEx

--- MIS_Encryption_Func.cs
+++ MIS_Encryption_Func.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
@@ -29,44 +30,61 @@ public class HttpEncryptFunction
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var data = JsonSerializer.Deserialize<EncryptRequest>(requestBody);
 
-            // Source blob client
-            var sourceBlobClient = new BlobClient(data.SourceConnectionString, data.SourceContainer, data.SourceBlobName);
+            // Validate pattern
+            if (string.IsNullOrEmpty(data.SourceBlobPattern))
+            {
+                var badResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                await badResponse.WriteStringAsync("SourceBlobPattern is required.");
+                return badResponse;
+            }
 
-            // Destination blob client
-            var destBlobClient = new BlobClient(data.DestConnectionString, data.DestContainer, data.DestBlobName);
-
-            // Public key blob client (assuming same storage account as dest, but can be separate)
-            var publicKeyBlobClient = new BlobClient(data.PublicKeyConnectionString, data.PublicKeyContainer, data.PublicKeyBlobName);
+            var sourceContainerClient = new BlobContainerClient(data.SourceConnectionString, data.SourceContainer);
+            var destContainerClient = new BlobContainerClient(data.DestConnectionString, data.DestContainer);
 
             // Download public key
-            using (MemoryStream publicKeyStream = new MemoryStream())
-            {
-                await publicKeyBlobClient.DownloadToAsync(publicKeyStream);
-                publicKeyStream.Position = 0;
+            var publicKeyBlobClient = new BlobClient(data.PublicKeyConnectionString, data.PublicKeyContainer, data.PublicKeyBlobName);
+            using var publicKeyStream = new MemoryStream();
+            await publicKeyBlobClient.DownloadToAsync(publicKeyStream);
 
-                // Download source blob
-                using (MemoryStream inputFileStream = new MemoryStream())
+            int matchCount = 0;
+
+            await foreach (var blobItem in sourceContainerClient.GetBlobsAsync())
+            {
+                if (IsMatch(blobItem.Name, data.SourceBlobPattern))
                 {
+                    _logger.LogInformation($"Matched blob: {blobItem.Name}");
+                    matchCount++;
+
+                    var sourceBlobClient = sourceContainerClient.GetBlobClient(blobItem.Name);
+                    var destBlobName = blobItem.Name + ".pgp";
+                    var destBlobClient = destContainerClient.GetBlobClient(destBlobName);
+
+                    using var inputFileStream = new MemoryStream();
                     await sourceBlobClient.DownloadToAsync(inputFileStream);
                     inputFileStream.Position = 0;
 
-                    // Encrypt
-                    using (MemoryStream encryptedStream = new MemoryStream())
-                    {
-                        using (PGP pgp = new PGP())
-                        {
-                            await pgp.EncryptStreamAsync(inputFileStream, encryptedStream, publicKeyStream, true, true);
-                            encryptedStream.Position = 0;
+                    using var encryptedStream = new MemoryStream();
+                    publicKeyStream.Position = 0;
 
-                            // Upload encrypted blob
-                            await destBlobClient.UploadAsync(encryptedStream, overwrite: true);
-                        }
+                    using (var pgp = new PGP())
+                    {
+                        await pgp.EncryptStreamAsync(inputFileStream, encryptedStream, publicKeyStream, true, true);
+                        encryptedStream.Position = 0;
+
+                        await destBlobClient.UploadAsync(encryptedStream, overwrite: true);
                     }
                 }
             }
 
             var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-            await response.WriteStringAsync($"Encrypted file uploaded to {data.DestContainer}/{data.DestBlobName}");
+            if (matchCount == 0)
+            {
+                await response.WriteStringAsync("No files matched the given pattern.");
+            }
+            else
+            {
+                await response.WriteStringAsync($"Encrypted {matchCount} file(s) matching pattern '{data.SourceBlobPattern}' uploaded to {data.DestContainer}.");
+            }
             return response;
         }
         catch (Exception ex)
@@ -78,18 +96,28 @@ public class HttpEncryptFunction
         }
     }
 
+    // Wildcard pattern matcher using regex
+    private bool IsMatch(string fileName, string pattern)
+    {
+        var regexPattern = "^" + Regex.Escape(pattern)
+                                     .Replace("\\*", ".*")
+                                     .Replace("\\?", ".") + "$";
+        return Regex.IsMatch(fileName, regexPattern, RegexOptions.IgnoreCase);
+    }
+
+    // Request body contract
     public class EncryptRequest
     {
         public string SourceConnectionString { get; set; }
         public string SourceContainer { get; set; }
-        public string SourceBlobName { get; set; }
-        
+
         public string DestConnectionString { get; set; }
         public string DestContainer { get; set; }
-        public string DestBlobName { get; set; }
 
         public string PublicKeyConnectionString { get; set; }
         public string PublicKeyContainer { get; set; }
         public string PublicKeyBlobName { get; set; }
+
+        public string SourceBlobPattern { get; set; } // e.g. "E*.csv", "*E.txt"
     }
 }
